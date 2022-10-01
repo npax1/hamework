51704a5ae7caf749851c84ef13a2d206513fee12
npax1 <prakh55555@gmail.com>
2022-10-01 13:29:43 +0300

    ex_1+

---
+++ ex_1.py
@@ -0,0 +1,14 @@
+start=int(input("веди число і"))
+end=int(input("веди число"))
+a = int(input("1Усі числа діапазону\n2. Усі числа діапазону за спаданням.\n3. Усі числа, кратні 7\n4. Кількість чисел, кратних 5."))
+if a ==1:
+    while start <= end:
+        print(start)
+        start=start+1
+if a ==2:
+    temp=start
+    start=end
+    end=temp
+    while start>end:
+        start-=1
+        print(f"{start}")
