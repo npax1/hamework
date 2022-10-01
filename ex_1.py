ab9d95634d39359a1dcea9915bc5173893ea34a0
npax1 <prakh55555@gmail.com>
2022-10-01 13:54:55 +0300

    ex_2

--- ex_1.py
+++ ex_1.py
@@ -13,3 +13,23 @@ if a ==2:
         start-=1
         print(f"{start}")
 if a == 3:
+    if start < 7:
+        start += (7 % start)
+    elif start > 7:
+        num1 = (start - (start % 7)) + 7
+    number3 = num1
+
+    while number3 <= end:
+        print(f'{number3}')
+        number3 += 7
+if a == 4 :
+    if  start < 5:
+        start+=(5%start)
+    elif start>5:
+        start=(start-(start%5))+5
+    number4=start
+
+    while number4<=end:
+        print(f"{number4}")
+        number4+=7
+
